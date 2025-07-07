d0db005bfced6bfcf3b6a092cf9f75897e987053
taliapannell <talia.pannell@accenture.com>
2025-07-07 17:05:53 -0400

    rearanged testimonials, changed carlyle title

--- src/pages/TestimonialsPage/TestimonialsPage.tsx
+++ src/pages/TestimonialsPage/TestimonialsPage.tsx
@@ -27,45 +27,45 @@ interface Testimonial {
 const testimonials: Testimonial[] = [
   {
     id: 1,
-    name: "Carlyle Campbell",
-    role: "Welcome Host",
-    text: "I met Talia one day and she's been nothing short of pleasant and super helpful. The best part though is how Talia handles the group; she absolutely shines when explaining how it works, what it's used for, it's capabilities and its limits. Talia is always the best and brightest part of our tours.",
-    avatarUrl: carlyleImage
+    name: "Jennifer Malach",
+    role: "Inclusion and Diversity Senior Manager",
+    text: "Thank you so much for sharing your expertise at our event yesterday! Hudson was a big hit – but only because of your presentation and active engagement with the educators in the room. Helping them experience first-hand the type of work we do was a crucial part of this program and I really appreciate you taking the time to join.",
+    avatarUrl: jenniferImage
   },
   {
     id: 2,
-    name: "Daniel Steinberg",
-    role: "Advanced App Engineering Specialist",
-    text: "Talia's enthusiasm is infectious, and she is incredibly proactive and reliable in achieving her goals. Her communication skills are exceptional. Talia is a skilled developer and quick learner. When I first met Talia, she was still relatively new to coding, but by last summer, she had already become a very strong front-end programmer.",
-    avatarUrl: danielImage
-  },
-  {
-    id: 3,
-    name: "Lester Bravo",
-    role: "Full Stack Engineering Specialist",
-    text: "Talia has demonstrated great versatility and commitment in her role within the team. She has shown a willingness to take on both frontend and backend development, showcasing her adaptability and a strong sense of collaboration. Her contributions have been invaluable, and her proactive attitude has positively impacted the team's progress.",
-    avatarUrl: lesterImage
-  },
-  {
-    id: 4,
-    name: "Silvia Chorny",
-    role: "HR Service Delivery Senior Analyst",
-    text: "Your story was an inspiration for anyone interested in starting their careers with us, here at Accenture. Your hard work and dedication made a huge impact. Looking back on the work that you have done, we hope that you can now see yourself as an example to all future apprentices. We are so proud of you and we hope you are too!",
-    avatarUrl: silviaImage
-  },
-  {
-    id: 5,
     name: "Evelyn Stellara",
     role: "Advanced App Engineering Specialist",
     text: "Talia has shown great progress in her programming skills and has become proficient in delivering tasks based on requirements. Her ability to work efficiently is evident from her recent project delivery, showing commendable adaptability in transitioning into a new project role and consistently delivering work within the given timeline.",
     avatarUrl: evelynImage
   },
   {
+    id: 3,
+    name: "Daniel Steinberg",
+    role: "Advanced App Engineering Specialist",
+    text: "Talia's enthusiasm is infectious, and she is incredibly proactive and reliable in achieving her goals. Her communication skills are exceptional. Talia is a skilled developer and quick learner. When I first met Talia, she was still relatively new to coding, but by last summer, she had already become a very strong front-end programmer.",
+    avatarUrl: danielImage
+  },
+  {
+    id: 4,
+    name: "Lester Bravo",
+    role: "Full Stack Engineering Specialist",
+    text: "Talia has demonstrated great versatility and commitment in her role within the team. She has shown a willingness to take on both frontend and backend development, showcasing her adaptability and a strong sense of collaboration. Her contributions have been invaluable, and her proactive attitude has positively impacted the team's progress.",
+    avatarUrl: lesterImage
+  },
+  {
+    id: 5,
+    name: "Silvia Chorny",
+    role: "HR Service Delivery Senior Analyst",
+    text: "Your story was an inspiration for anyone interested in starting their careers with us, here at Accenture. Your hard work and dedication made a huge impact. Looking back on the work that you have done, we hope that you can now see yourself as an example to all future apprentices. We are so proud of you and we hope you are too!",
+    avatarUrl: silviaImage
+  },
+  {
     id: 6,
-    name: "Jennifer Malach",
-    role: "Inclusion and Diversity Senior Manager",
-    text: "Thank you so much for sharing your expertise at our event yesterday! Hudson was a big hit – but only because of your presentation and active engagement with the educators in the room. Helping them experience first-hand the type of work we do was a crucial part of this program and I really appreciate you taking the time to join.",
-    avatarUrl: jenniferImage
+    name: "Carlyle Campbell",
+    role: "Accenture Alumni",
+    text: "I met Talia one day and she's been nothing short of pleasant and super helpful. The best part though is how Talia handles the group; she absolutely shines when explaining how it works, what it's used for, it's capabilities and its limits. Talia is always the best and brightest part of our tours.",
+    avatarUrl: carlyleImage
   }
 ];
 
